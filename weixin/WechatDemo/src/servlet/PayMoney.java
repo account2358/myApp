f6c8e7d15519398f19f6d2b4c48c35f3b24bd297
gw <a>
2017-12-28 00:16:48 +0800

    增加pay回调

--- weixin/WechatDemo/src/servlet/PayMoney.java
+++ weixin/WechatDemo/src/servlet/PayMoney.java
@@ -73,8 +73,9 @@ public class PayMoney extends HttpServlet {
         
         // 微信加密签名  
         String paymoney = request.getParameter("PayMoney");  
-
     	String machineNumber = request.getParameter("MachineNumber"); 
+    	String ip = getRemoteAddress(request);  
+        System.out.println("ip:" + ip);  
         System.out.println("paymoney:" + paymoney);
         System.out.println("machineNumber:" + machineNumber);
     	String respContent = "";
@@ -112,5 +113,19 @@ public class PayMoney extends HttpServlet {
         out.print(respMessage);  
         out.close();  
     }  
+    
+    public String getRemoteAddress(HttpServletRequest request) {  
+        String ip = request.getHeader("x-forwarded-for");  
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+            ip = request.getHeader("Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+            ip = request.getHeader("WL-Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+            ip = request.getRemoteAddr();  
+        }  
+        return ip;  
+    } 
   
 }  
